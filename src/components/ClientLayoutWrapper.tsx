bbd8b9c1612bc98c68e2c34767db0b602bf9a1e0
ashiqkodappully <ashiqdink@gmail.com>
2025-05-28 18:21:28 +0000

    footer

--- src/components/ClientLayoutWrapper.tsx
+++ src/components/ClientLayoutWrapper.tsx
@@ -2,7 +2,6 @@
 
 import React from "react";
 import { Providers } from "@/app/providers";
-import Footer from "@/components/Footer";
 import { TempoInit } from "@/app/tempo-init";
 
 export default function ClientLayoutWrapper({
@@ -15,7 +14,6 @@ export default function ClientLayoutWrapper({
       <div className="flex flex-col min-h-screen">
         <TempoInit />
         <main className="flex-1">{children}</main>
-        <Footer />
       </div>
     </Providers>
   );
