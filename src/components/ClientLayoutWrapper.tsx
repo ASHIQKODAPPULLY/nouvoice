6c875a60942e3ec581934d1c7d6cf2d198b8dfc5
ashiqkodappully <ashiqdink@gmail.com>
2025-05-30 13:30:30 +0000

    double up

--- src/components/ClientLayoutWrapper.tsx
+++ src/components/ClientLayoutWrapper.tsx
@@ -4,7 +4,6 @@ import React from "react";
 import { Providers } from "@/app/providers";
 import { TempoInit } from "@/app/tempo-init";
 import Footer from "./Footer";
-import MobileMenu from "./MobileMenu";
 
 export default function ClientLayoutWrapper({
   children,
@@ -15,9 +14,6 @@ export default function ClientLayoutWrapper({
     <Providers>
       <div className="flex flex-col min-h-screen">
         <TempoInit />
-        <div className="fixed top-0 right-0 z-50">
-          <MobileMenu />
-        </div>
         <main className="flex-1">{children}</main>
         <Footer />
       </div>
