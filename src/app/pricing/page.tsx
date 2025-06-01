2eea9772a53bcdbc919b5eaa5cc7ab170f2ef105
ashiqkodappully <ashiqdink@gmail.com>
2025-06-01 04:31:42 +0000

    gg

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -50,6 +50,7 @@ export default function PricingPage() {
         headers: {
           "Content-Type": "application/json",
         },
+        credentials: "include", // Send cookies with the request
         body: JSON.stringify({
           priceId,
           returnUrl: window.location.origin,
